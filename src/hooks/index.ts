a48d5fd0a86d122cdce457c691f8593d1b5f5daa
Jake Gaylor <jhgaylor@gmail.com>
2025-07-07 00:49:41 -0500

    remove hooks from a pre-webext-redux world

--- src/hooks/index.ts
+++
@@ -1,4 +0,0 @@
-export { useFrameMessaging } from './useFrameMessaging';
-export { useSidePanelMessaging } from './useSidePanelMessaging';
-export { usePopupMessaging } from './usePopupMessaging';
-export { useContentScriptMessaging } from './useContentScriptMessaging';
